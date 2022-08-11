3e6ae1a3c02b68ee3cd2d9ad7c359d84f1066dd0
Imanol Aranburu <iaranburu@binovo.es>
2022-08-11 13:33:08 +0200

    [REF] 10440 account_lock_to_date not installable

--- account_lock_to_date/__manifest__.py
+++ account_lock_to_date/__manifest__.py
@@ -9,7 +9,7 @@
     'license': 'AGPL-3',
     'author': 'ForgeFlow, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/account-financial-tools',
-    'installable': True,
+    'installable': False,
     'depends': [
         'account',
     ],
